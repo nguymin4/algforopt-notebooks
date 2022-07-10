ab6f6d54c60fed968aad61b4beac99e7bfcc3d06
Minh Son Nguyen <minh.son.nguyen.1209@gmail.com>
2022-07-10 14:41:43 +0300

    feat: add wheeler ridge test function

--- lib/test_functions.jl
+++ lib/test_functions.jl
@@ -11,4 +11,15 @@ function rosenbrock(a::Real, b::Real, c=1::Real)
     return f, ∇f
 end
 
+function wheeler(a=1.5)
+    f(x::Vector{Float64}) = -exp(-(x[1] * x[2] - a) ^ 2  - (x[2] - a) ^ 2)
+    function ∇f(x::Vector{Float64})
+        return [
+            -2 * y * f(x) * (x[1] * x[2] - a),
+            -2 * f(x) * (x[2] * (x[1] ^ 2 + 1) - a * (x[1] + 1)),
+        ]
+    end
+    return f, ∇f
+end
+
 end
